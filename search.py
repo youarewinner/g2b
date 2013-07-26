400be49f912eac4cf7b30fb5e2516e391feab431
youarewinner <youarewinner@naver.com>
2013-07-26 16:09:11 +0900

    test

---
+++ search.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-  test
+import urllib.request
+import re
+
+html_content = urllib.request.urlopen('http://www.kict.re.kr/')
+print(html_content.read())
+
+matches = re.findall('Home', html_content);
+
+if len(matches) == 0: 
+   print ('I did not find anything')
+else:
+   print ('My string is in the html')
