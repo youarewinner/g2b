ba3f77e3074fb09ec5456031e584e49be5c8ec6e
Jonghoon lee <youarewinner@naver.com>
2013-07-27 16:00:30 +0900

    hangul

--- html-webtoon.py
+++ html-webtoon.py
@@ -1,6 +1,6 @@
 # python 2.6
 # http://toors.tistory.com/entry/Python-BeautifulSoup%EC%9C%BC%EB%A1%9C-%EA%B2%8C%EC%8B%9C%ED%8C%90-%EA%B8%81%EC%96%B4%EC%98%A4%EA%B8%B0-%ED%8C%A8%ED%84%B4
-##
+## 이글 은 한글 연습니다.
 import urllib
 import BeautifulSoup
 
